ad895a90b82d9376f253ea39300e110e3a460983
Alexandre Kaspar <xion.luhnis@gmail.com>
2014-08-11 23:07:58 -0400

    Stack and heap, fibonacci sequence

---
+++ slides/code/heap_fibseq.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdlib>
+
+// fibonacci sequence
+int *fibSequence(int N){
+  int *seq = new int[N];
+  seq[0] = seq[1] = 1;
+  for(int i = 2; i < N; ++i)
+    seq[i] = seq[i-1] + seq[i-2];
+  return seq;
+}
+
+int main(int argc, char **argv){
+  if(argc != 2){
+    std::cerr << "Usage: ./fibonacci N\n";
+    return 1;
+  }
+  int N = atoi(argv[1]);
+  int *seq = fibSequence(N);
+
+  // display the sequence
+  for(int i = 0; i < N; ++i){
+    std::cout << "fib(" << i << ") = " << seq[i] << "\n";
+  }
+  return 0;
+}
